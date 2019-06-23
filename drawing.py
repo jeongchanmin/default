e87511facf39f256571329cc7e98bae7dea0dcdd
jeongchanmin <51259588+jeongchanmin@users.noreply.github.com>
2019-06-23 16:01:34 +0900

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -12,10 +12,10 @@ canvas = tkinter.Canvas(window, width = 500, height = 230, relief = "solid", bd 
 canvas.pack(side = "top")
 canvas.place(x = 70, y = 10)
 
-button1 = tkinter.Button().pack()       #빨간색
-button2 = tkinter.Button().pack()       #파란색
-button3 = tkinter.Button().pack()       #검정색
-button4 = tkinter.Button().pack()       #흰색
+button1 = tkinter.Button(relief = "solid", width = 20, text = "red").place(height = 30, x = 260, y = 247)        #빨간색
+button2 = tkinter.Button(relief = "solid", width = 20, text = "blue").place(height = 30, x = 260, y = 278)       #파란색
+button3 = tkinter.Button(relief = "solid", width = 20, text = "black").place(height = 30, x = 260, y = 309)       #검정색
+button4 = tkinter.Button(relief = "solid", width = 20, text = "white").place(height = 30, x = 260, y = 340)       #흰색
 
 
 window.mainloop()
