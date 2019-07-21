b3a778b306e3762ffcc4fda1246ca936221e3a3e
jeongchanmin <51259588+jeongchanmin@users.noreply.github.com>
2019-07-21 13:40:43 +0900

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import ttk               
 
 #메인 설정
-window=tkinter.Tk()                                                                                                     #창 띄우기
+window=tkinter.Tk()                                                                                                      #창 띄우기
 
 window.title("그림판")                                                                                                   #창 이름
 window.geometry("640x400+100+100")                                                                                       #창 크기
@@ -24,8 +24,9 @@ def up(event):
   global x0, y0
   if (x0,y0) == (event.x, event.y):
     canvas.create_line(x0, y0, x0+1, y0+1)
+
 #캔버스 설정
-canvas = tkinter.Canvas(window, width = 500, height = 230, relief = "solid", bd = 1) #캔버스 크기, 창의 테두리 
+canvas = tkinter.Canvas(window, width = 500, height = 230, relief = "solid", bd = 1)                                  #캔버스 크기, 창의 테두리 
 canvas.pack(side = "top")                                                                                             #캔버스 위치
 canvas.place(x = 70, y = 10)                                                                                          #캔버스 위치
 
